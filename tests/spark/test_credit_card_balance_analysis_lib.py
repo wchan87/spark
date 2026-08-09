1206296836199985285363f59410910860b2ea4b
Wren Chan <wlechan87@gmail.com>
2026-08-09 13:28:34 -0400

    Confirmed that external libraries can be intercepted so import failure doesn't happen

---
+++ tests/spark/test_credit_card_balance_analysis_lib.py
@@ -0,0 +1,32 @@
+from unittest.mock import patch, MagicMock
+from pandas import DataFrame
+from pyspark.sql import SparkSession
+import sys
+
+
+# Intercept the import of fred before it happens in src.spark.credit_card_balance_analysis_lib
+sys.modules["fred"] = MagicMock()
+sys.modules["fred.utility"] = MagicMock()
+from src.spark.credit_card_balance_analysis_lib import (
+    main
+)
+
+
+@patch("src.spark.credit_card_balance_analysis_lib.calculate_credit_card_payment")
+@patch("src.spark.credit_card_balance_analysis_lib.SparkSession")
+def test_main(
+        mock_spark_session: MagicMock,
+        mock_calculate_credit_card_payment: MagicMock,
+        spark: SparkSession,
+        payment_dataframe: DataFrame
+    ):
+    # ARRANGE
+    # Intercept the spark_session created in the code with the fixture
+    mock_spark_session.builder.appName.return_value.getOrCreate.return_value = spark
+    mock_calculate_credit_card_payment.return_value = payment_dataframe
+
+    # ACT
+    main()
+
+    # ASSERT
+    mock_calculate_credit_card_payment.assert_called_once_with(spark)
